0ca7aeab441fbd000dbb2fc235c72e892617e09e
Elizabeth <dark.ely@gmail.com>
2023-03-06 20:20:07 -0300

    diez

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,19 @@
 // conn PG
 const { Pool } = require('pg')
 
+const port =process.env.PGPORT || 5432;
+const host =process.env.PGHOST || 'localhost';
+const user =process.env.PGUSER || 'postgres';
+const database =process.env.PGDATABASE || 'arriendo';
+const password =process.env.PGPASSWORD || 'admin';
+
 //let pool;
 const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'admin',
-    database: 'arriendo',
-    port: '5432',
+    host: host,
+    user: user,
+    password: password,
+    database: database,
+    port: port,
     allowExitOnIdle: true
     })
 
